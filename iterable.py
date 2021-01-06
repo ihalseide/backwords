6cc10093b36a1045e50f1b31e2b43af34ba73dc3
Izak Halseide <imitatio-ignotum@protonmail.com>
2021-01-06 01:14:27 -0700

    Remove unnecessary code

--- iterable.py
+++
@@ -1,9 +0,0 @@
-
-def iterable (x) -> bool:
-    '''Return whether a given value is iterable
-    through the iter(...) function'''
-    try:
-        iter(x)
-        return True
-    except TypeError:
-        return False
