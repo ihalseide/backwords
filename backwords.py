e88e2bb5b7089dece10101c2641705877be9ed97
ihalseide <imitatio-ignotum@protonmail.com>
2021-02-26 20:40:11 -0700

    Fix up code
    Use sys.stdin.read instead of the insane getch function
    
    Removed mulitple debug levels

--- backwords.py
+++ backwords.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
 
-import sys
-from getch import getch
+import sys, argparse
 
-V_TRUE = 255
-V_FALSE = 0
 DIGITS = '0123456789ABCDEF'
-debug = False
-debug2 = False
 
-if len(sys.argv) != 2:
-    print('Usage:')
-    print(sys.argv[0], '[filename]')
-    sys.exit(-1)
+parser = argparse.ArgumentParser()
+parser.add_argument('file')
+args = parser.parse_args()
 
-filename = sys.argv[1]
-with open(filename, 'r') as f:
+with open(args.file, 'r') as f:
     program = f.read() 
 
-ip = 0 # instruction pointer 
-stack = bytearray([])
-tape = bytearray([0 for x in range(256)])
-memory_page = 0
+debug = False 
+ip = 0                # instruction pointer 
+stack = bytearray([]) # data stack 
+
+# memory
+tape = bytearray([0 for x in range(256)]) 
+page = 0
 
 def pop ():
     return stack.pop()
@@ -35,8 +31,7 @@ def tape_expand (index_required):
         tape += bytearray([0 for x in range(256)])
 
 def execute (x):
-    global tape, stack, ip, memory_page
-
+    global tape, stack, ip, page, running 
     if ':' == x:
         # dup 
         if stack:
@@ -74,18 +69,14 @@ def execute (x):
     elif ',' == x:
         # emit
         val = pop()
-        char = chr(val)
-        print(end=char)
+        sys.stdout.write(chr(val))
     elif '?' == x:
         # input
-        c = getch() 
+        c = sys.stdin.read(1)
         push(ord(c))
-        if c == b'\x03':
-            # ^C received
-            raise KeyboardInterrupt()
     elif ';' == x:
         # halt the outer infinite loop
-        raise SystemExit()
+        running = False
     elif '\\' == x:
         # Loop back
         ip = -1 # it will be incremented to zero in an outer loop
@@ -103,15 +94,15 @@ def execute (x):
         # equal
         a, b = pop(), pop()
         c = a == b
-        push(V_TRUE if c else V_FALSE)
+        push(255 if c else 0)
     elif '>' == x:
         top, top2 = pop(), pop()
         c = top2 > top
-        push(V_TRUE if c else V_FALSE)
+        push(255 if c else 0)
     elif '<' == x:
         top, top2 = pop(), pop()
         c = top2 < top
-        push(V_TRUE if c else V_FALSE)
+        push(255 if c else 0)
     elif '.' == x:
         # eval
         execute(chr(pop()))
@@ -122,37 +113,39 @@ def execute (x):
         # branch backwards
         ip -= pop() + 1
     elif 'n' == x:
-        # conditional
-        do_skip = pop() == V_FALSE
+        # Conditional (is not zero)
+        do_skip = pop() == 0
         if do_skip:
             ip += 1
     elif 'z' == x:
-        # inverted conditional
-        do_skip = pop() != V_FALSE
+        # Inverted conditional
+        do_skip = pop() != 0
         if do_skip:
             ip += 1
     elif 'u' == x.lower():
         # Clear the stack
         stack = []
     elif 's' == x.lower():
-        # swap 
+        # Swap 2 numbers on the stack
         a, b = pop(), pop()
         push(a)
         push(b)
     elif '{' == x:
-        memory_page -= 1
+        # Previous memory page
+        page -= 1
     elif '}' == x:
-        memory_page += 1
+        # Next memory page
+        page += 1
     elif '@' == x:
         # Tape fetch
-        index = memory_page * 256 + pop()
+        index = page * 256 + pop()
         if index not in range(len(tape)):
             tape_expand()
         push(tape[index])
     elif '!' == x:
         # Tape store
         index, value = pop(), pop()
-        index += memory_page * 256
+        index += page * 256
         if index not in range(len(tape)):
             tape_expand()
         tape[index] = value 
@@ -169,54 +162,39 @@ def execute (x):
         push(len(stack))
     elif 'g' == x.lower():
         # Print debug info
-        print('debug: [%s]' %(','.join(str(x) for x in stack)))
+        print('stack [%s]' %(','.join(str(x) for x in stack)))
     elif 'k' == x.lower():
         # Debug "breakpoint"
-        input('pause:')
+        input('pause...')
     elif '"' == x:
         # Read a string
-        # TODO: add escape codes
         char = ''
         while char != '"':
             if char:
+                if '\\' == char:
+                    escape = True
+                    ip += 1
+                    char = program[ip]
                 push(ord(char))
             ip += 1
             char = program[ip]
     else:
-        # Ignore characters that aren't assigned meaning
+        # Ignore other characters
         pass
         
-exception = False
-ex = None
 try:
-    while True: 
-        if debug2:
-            print('ip:', ip)
-            print('stack:', stack)
-            print('program[ip]:', program[ip])
-            print('-------------------------')
-            input('{') 
-        # The program counter automatically loops back around
+    running = True
+    while running: 
+        # The program counter automatically loops back around,
+        # and the empty program will run an infinite loop 
         if ip >= len(program):
             ip = 0
-            continue # This continue makes the empty program run an infinite loop 
+            continue
         x = program[ip]  
         execute(x)
         ip += 1
 except Exception as e:
-    exception = True
-    ex = e
-finally:
     if debug:
-        print()
-        if exception: 
-            print('----- exception -----')
-        else:
-            print('----- success!! -----')
-        print('ip:', ip)
-        print('stack:', stack)
-        print('program[ip]:', program[ip])
-        print('-------------------------')
-        if exception:
-            raise ex
-
+        raise e
+    print('Segmentation fault'
+    sys.exit(-1)
